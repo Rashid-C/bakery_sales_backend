f5e80229e10d839b42c1b116f096c08d4b2776c5
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-12-29 00:25:31 +0530

    User created by admin

--- frontend/services/auth.service.ts
+++ frontend/services/auth.service.ts
@@ -1,15 +1,15 @@
+// services/auth.service.ts
 import api from "../api/api";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const login = async (username: string, password: string) => {
-    const res = await api.post("/login", { username, password });
+    const res = await api.post("/auth/login", {
+        username,
+        password,
+    });
 
     await AsyncStorage.setItem("token", res.data.token);
     await AsyncStorage.setItem("role", res.data.role);
 
     return res.data;
-};
-
-export const logout = async () => {
-    await AsyncStorage.clear();
 };
